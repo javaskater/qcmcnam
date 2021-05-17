0e37484ac6b4969ee369cf80734ae82cdfb91de9
JP Mena <javaskater@jpmena.eu>
2021-05-17 19:35:04 +0200

    mise à niveau du shéma Mysql

--- scripts/sql/classe.sql
+++ scripts/sql/classe.sql
@@ -1,3 +1,5 @@
 CREATE TABLE `qcmcnam`.`classe` ( `id` INT NOT NULL AUTO_INCREMENT , `nom` VARCHAR(100) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB; 
 INSERT INTO `classe` (`id`, `nom`) VALUES (NULL, 'IGPDE');
 INSERT INTO `classe` (`id`, `nom`) VALUES (NULL, 'NOISIEL');
+
+CREATE TABLE `qcmcnam`.`qcmclasse` ( `idClasse` INT NOT NULL, `idQcm` INT NOT NULL) ENGINE = InnoDB; 
